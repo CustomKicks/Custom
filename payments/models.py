5a4b80c9c553634cd836f61aae31413b7f10cce5
Matidza <Matidza46@gmail.com>
2024-11-24 17:31:34 +0200

    Shipping Info Form

--- payments/models.py
+++ payments/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class ShippingAddress(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    full_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    address1 = models.CharField(max_length=255)
-    address2 = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    province = models.CharField(max_length=255, null=True, blank=True)
-    zipcode = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=255)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	shipping_full_name = models.CharField(max_length=255)
+	shipping_email = models.CharField(max_length=255)
+	shipping_address1 = models.CharField(max_length=255)
+	shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
+	shipping_city = models.CharField(max_length=255)
+	shipping_province = models.CharField(max_length=255, null=True, blank=True)
+	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
+	shipping_country = models.CharField(max_length=255)
 
-    class Meta:
-        verbose_name_plural = "Shipping Address"
 
-    def __str__(self):
-        return f'Shipping Addres - {str(self.id)}'
+	# Don't pluralize address
+	class Meta:
+		verbose_name_plural = "Shipping Address"
+
+	def __str__(self):
+		return f'Shipping Address - {str(self.id)}'
