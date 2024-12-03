358491beffe6698efa6ae99a8f7d621871539001
CustomKicks <Customkicks.django@gmail.com>
2024-12-03 21:09:10 +0200

    Update settings.py

--- Customs/settings.py
+++ Customs/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 
 # Railway.app
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.('POSTGRES_CONNECTION_STRING'), conn_max_age=600)
+    'default': dj_database_url.parse(os.environ['POSTGRES_CONNECTION_STRING'], conn_max_age=600)
 }
 
 
