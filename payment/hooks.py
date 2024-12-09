60f0cb67a8e6ab08d1b2fd33f3676d56672ae7db
CustomKicks <Customkicks.django@gmail.com>
2024-12-09 02:42:09 +0200

    Create hooks.py

---
+++ payment/hooks.py
@@ -0,0 +1,9 @@
+from paypal.standard.ipn.models import ST_COMPLETED 
+from paypal.standard.ipn.signals import valid_ipn_received 
+from django.dispatch import receiver 
+from django.contrib.auth.models import User 
+from django.conf import settings  
+
+@receiver(valid_ipn_received) 
+def payment_notification(sender, **kwargs): 
+    ipn_obj = sender 
